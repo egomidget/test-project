4b0e8e7b27dcc0e349f783475e827e458c6c1936
RZicman <zicmanjo@gmail.com>
2026-04-10 10:03:18 +0100

    FIX return in divide func and add func name

--- calculator.py
+++ calculator.py
@@ -1,13 +1,13 @@
 # here another comment
-def add_numbers(a, b):
+def add(a, b):
     return a + b
 
 def subtract(a, b):
-    return a + b
+    return a - b
 
 def multiply(a, b):
     return a * b
 
 def divide(a, b):
-    retun a / b
+    return a / b
 #this is a commment
